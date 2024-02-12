e99b728bcbbec0f547f304c4ac0df5cc080e841f
mai <m.kanda.cingroup2012@gmail.com>
2024-02-12 22:24:07 +0900

    first commit

---
+++ lesson3.php
@@ -0,0 +1,18 @@
+﻿<?php
+// 100円のチョコレートを1個
+// 50円のアメを3個買いました。
+
+// 各値段の計算を変数に代入し、
+// その合計を$sumと言う変数に代入して
+// 「○○○円です。」という形で出力してください。
+
+$chocolate_price = 100 * 1;
+
+$candy_price = 50 * 3;
+
+$sum = $chocolate_price + $candy_price;
+
+$output = $sum . "円です。";
+
+echo $output;
+
